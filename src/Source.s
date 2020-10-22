85e68ce6ae819233c9cafaf96211a750eb6c745f
MashrurIslam <71884604+MashrurIslam@users.noreply.github.com>
2020-10-22 13:20:24 +0300

    Add files via upload
    Second Commit

---
+++ src/Source.s
@@ -0,0 +1,71 @@
+sort(int*, int):
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD PTR [rbp-24], rdi
+        mov     DWORD PTR [rbp-28], esi
+        mov     DWORD PTR [rbp-4], 0
+.L6:
+        mov     eax, DWORD PTR [rbp-28]
+        sub     eax, 1
+        cmp     DWORD PTR [rbp-4], eax
+        jge     .L7
+        mov     DWORD PTR [rbp-8], 0
+.L5:
+        mov     eax, DWORD PTR [rbp-28]
+        sub     eax, 1
+        sub     eax, DWORD PTR [rbp-4]
+        cmp     DWORD PTR [rbp-8], eax
+        jge     .L3
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rax]
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        add     rax, 1
+        lea     rcx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rcx
+        mov     eax, DWORD PTR [rax]
+        cmp     edx, eax
+        jle     .L4
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rbp-12], eax
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        add     rax, 1
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rbp-8]
+        movsx   rdx, edx
+        lea     rcx, [0+rdx*4]
+        mov     rdx, QWORD PTR [rbp-24]
+        add     rdx, rcx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rdx], eax
+        mov     eax, DWORD PTR [rbp-8]
+        cdqe
+        add     rax, 1
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rdx, rax
+        mov     eax, DWORD PTR [rbp-12]
+        mov     DWORD PTR [rdx], eax
+.L4:
+        add     DWORD PTR [rbp-8], 1
+        jmp     .L5
+.L3:
+        add     DWORD PTR [rbp-4], 1
+        jmp     .L6
+.L7:
+        nop
+        pop     rbp
+        ret
